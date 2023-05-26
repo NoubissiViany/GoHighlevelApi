17b71a3b22301f07507ab6f62925748bb9b380db
NNVG <g696607013@gmail.com>
2023-05-26 20:13:04 +0100

    Added GHLAPI

--- post.js
+++ post.js
@@ -1,9 +1,25 @@
-var express = require("express");
-var app = express();
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const dotEnv = require("dotenv");
+dotEnv.config();
 
+const app = express();
 var axios = require("axios");
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
+app.use(logger("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
 
 app.post("/", function (req, res) {
   Points = req.body["Total Point Of All"];
@@ -60,8 +76,15 @@ app.post("/", function (req, res) {
     });
 });
 
-var server = app.listen(8000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log("Example app listening at http://%s:%s", host, port);
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
+
+module.exports = app;
